3f5d92c9fc5672af66cdd8cccd91ae3c7aab2ded
Park, Sojeong <sojeong2101@gmail.com>
2023-11-05 07:04:45 +0900

    fix: OrderDto 변수 타입 수정

--- 박소정/ShoppingMall/src/main/java/com/example/shoppingmall/dto/OrderDto.java
+++ 박소정/ShoppingMall/src/main/java/com/example/shoppingmall/dto/OrderDto.java
@@ -1,5 +1,7 @@
 package com.example.shoppingmall.dto;
 
+import java.util.ArrayList;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -11,7 +13,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class OrderDto {
     private Integer id;
-    private String customerName;
-    private String itemName;
+    private List<CustomerDto> customers = new ArrayList<>();
+    private List<ItemDto> items = new ArrayList<>();
     private Integer count;
 }
