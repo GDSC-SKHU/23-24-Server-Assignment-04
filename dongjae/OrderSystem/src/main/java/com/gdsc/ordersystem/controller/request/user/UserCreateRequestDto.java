00e202d0e4fcb208032653ea22dfb6e5bfd812d8
djdongjae <jk47wee@gmail.com>
2023-11-06 05:10:50 +0900

    rename: request에 도메인별 폴더 생성

---
+++ dongjae/OrderSystem/src/main/java/com/gdsc/ordersystem/controller/request/user/UserCreateRequestDto.java
@@ -0,0 +1,36 @@
+package com.gdsc.ordersystem.controller.request.user;
+
+import com.gdsc.ordersystem.domain.user.User;
+import com.gdsc.ordersystem.domain.user.UserType;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class UserCreateRequestDto {
+
+    @NotBlank
+    private String name;
+    @NotBlank
+    private String email;
+    @NotNull
+    private UserType userType;
+
+    @Builder
+    public UserCreateRequestDto(String name, String email, UserType userType) {
+        this.name = name;
+        this.email = email;
+        this.userType = userType;
+    }
+
+    public User toEntity() {
+        return User.builder()
+                .name(name)
+                .email(email)
+                .userType(userType)
+                .build();
+    }
+}
