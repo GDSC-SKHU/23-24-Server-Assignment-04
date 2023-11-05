9ff55321c2211c6ccdb755a10a98f6e6d28b3efc
djdongjae <jk47wee@gmail.com>
2023-11-06 05:25:16 +0900

    rename: response/user 폴더 생성

---
+++ dongjae/OrderSystem/src/main/java/com/gdsc/ordersystem/controller/response/user/UserDeleteResponseDto.java
@@ -0,0 +1,19 @@
+package com.gdsc.ordersystem.controller.response.user;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class UserDeleteResponseDto {
+
+    Long userId;
+
+    public static UserDeleteResponseDto of(Long userId){
+        return new UserDeleteResponseDto(userId);
+    }
+
+}
