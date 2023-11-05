8c34d24174d6e3a0cfdcfadf7af64e1174ae4e97
djdongjae <jk47wee@gmail.com>
2023-11-06 05:15:52 +0900

    refactor: 공통 URI 한번에 처리

--- dongjae/OrderSystem/src/main/java/com/gdsc/ordersystem/controller/UserController.java
+++ dongjae/OrderSystem/src/main/java/com/gdsc/ordersystem/controller/UserController.java
@@ -1,11 +1,11 @@
 package com.gdsc.ordersystem.controller;
 
 import com.gdsc.ordersystem.common.dto.ApiResponse;
-import com.gdsc.ordersystem.controller.request.UserCreateRequestDto;
-import com.gdsc.ordersystem.controller.request.UserNameUpdateDto;
-import com.gdsc.ordersystem.controller.request.UserTypeUpdateDto;
-import com.gdsc.ordersystem.controller.response.UserDeleteResponseDto;
-import com.gdsc.ordersystem.controller.response.UserResponseDto;
+import com.gdsc.ordersystem.controller.request.user.UserCreateRequestDto;
+import com.gdsc.ordersystem.controller.request.user.UserNameUpdateDto;
+import com.gdsc.ordersystem.controller.request.user.UserTypeUpdateDto;
+import com.gdsc.ordersystem.controller.response.user.UserDeleteResponseDto;
+import com.gdsc.ordersystem.controller.response.user.UserResponseDto;
 import com.gdsc.ordersystem.exception.SuccessCode;
 import com.gdsc.ordersystem.service.UserService;
 import jakarta.validation.Valid;
@@ -17,46 +17,47 @@ import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
+@RequestMapping("/api/users")
 public class UserController {
 
     private final UserService userService;
 
-    @PostMapping("/api/users")
+    @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public ApiResponse<UserResponseDto> createUser(@RequestBody @Valid final UserCreateRequestDto requestDto) {
         final UserResponseDto data = userService.save(requestDto);
         return ApiResponse.success(SuccessCode.CREATE_CUSTOMER_SUCCESS, data);
     }
 
-    @GetMapping("/api/users")
+    @GetMapping
     @ResponseStatus(HttpStatus.OK)
     public ApiResponse<List<UserResponseDto>> getUsers() {
         final List<UserResponseDto> data = userService.getUsers();
         return ApiResponse.success(SuccessCode.GET_CUSTOMER_SUCCESS, data);
     }
 
-    @GetMapping("/api/users/{id}")
+    @GetMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     public ApiResponse<UserResponseDto> getUser(@PathVariable final Long id) {
         final UserResponseDto data = userService.getUser(id);
         return ApiResponse.success(SuccessCode.GET_CUSTOMER_SUCCESS, data);
     }
 
-    @PatchMapping("/api/users/name/{id}")
+    @PatchMapping("/name/{id}")
     @ResponseStatus(HttpStatus.OK)
     public ApiResponse<UserResponseDto> updateUserName(@PathVariable final Long id, @RequestBody @Valid final UserNameUpdateDto requestDto) {
         final UserResponseDto data = userService.updateUserName(id, requestDto);
         return ApiResponse.success(SuccessCode.UPDATE_CUSTOMER_SUCCESS, data);
     }
 
-    @PatchMapping("/api/users/type/{id}")
+    @PatchMapping("/type/{id}")
     @ResponseStatus(HttpStatus.OK)
     public ApiResponse<UserResponseDto> updateUserType(@PathVariable final Long id, @RequestBody @Valid final UserTypeUpdateDto requestDto) {
         final UserResponseDto data = userService.updateUserType(id, requestDto);
         return ApiResponse.success(SuccessCode.UPDATE_CUSTOMER_SUCCESS, data);
     }
 
-    @DeleteMapping("/api/users/{id}")
+    @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     public ApiResponse<UserDeleteResponseDto> deleteUser(@PathVariable final Long id) {
         final UserDeleteResponseDto data = userService.deleteUser(id);
